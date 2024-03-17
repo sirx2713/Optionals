816e993727819ba5222778b045530faddcbabc9b
sirx2713 <bradx2713@gmail.com>
2024-03-17 18:14:44 +0300

    Initial Commit

---
+++ Optionals/OptionalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptionalsApp.swift
+//  Optionals
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/17.
+//
+
+import SwiftUI
+
+@main
+struct OptionalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
